143b4e5b1f187ff12856bce2ca720b769398828a
Abhay kholia <103960752+AbhayKholia@users.noreply.github.com>
2023-06-08 12:55:27 +0530

    Delete sameep - Copy/my-app directory

--- sameep - Copy/my-app/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import categoryReducer from "./categorySlice"
-const store = configureStore({
-    reducer : {
-        category :categoryReducer
-    }
-})
-export default store
